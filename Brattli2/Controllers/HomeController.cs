6ce6b0c1c7222d0c04a4a11c37a34314bb642548
lselbeck <lselbeck@gmail.com>
2015-07-25 12:41:16 -0700

    initial create

---
+++ Brattli2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brattli2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Welcome to Brattli Consulting";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "What We Do";
+            return View();
+        }
+
+        public ActionResult Performance()
+        {
+            ViewBag.Title = "Performance";
+            return View();
+        }
+
+        public ActionResult Quotes()
+        {
+            ViewBag.Title = "Client Quotes";
+            return View();
+        }
+
+        public ActionResult Loreen()
+        {
+            ViewBag.Title = "Loreen Brattli";
+            return View();
+        }
+
+        public ActionResult Oystein()
+        {
+            ViewBag.Title = "Oystein Brattli";
+            return View();
+        }
+
+        public ActionResult Principles()
+        {
+            ViewBag.Title = "Our Principles";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            return View();
+        }
+
+
+    }
+}
---
+++ Brattli2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brattli2.Startup))]
+namespace Brattli2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+        }
+    }
+}
